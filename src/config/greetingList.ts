e16ee8488b360e159dcb4e004855e2460329917f
はらたく <70054655+HRTK92@users.noreply.github.com>
2023-03-02 20:18:20 +0900

    挨拶のパターンを追加する

--- src/config/greetingList.ts
+++ src/config/greetingList.ts
@@ -1,7 +1,7 @@
 const greetingList = [
-  //
-  { date: '1/1', greeting: 'あけましておめでとうございます' }
-  
+  { date: '1/1', greeting: 'あけましておめでとうございます' },
+  { data: '12/24', greeting: '🎄メリークリスマス🎄' },
+  { data: '12/31', greeting: '良いお年を！'}
 ]
 
 export default greetingList
